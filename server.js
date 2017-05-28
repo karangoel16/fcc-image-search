4f654f17e1a13eb3f385a4417c8d5b2fe44d05cc
karangoel16 <karangoel16@yahoo.com>
2017-05-28 20:09:20 +0530

    Some minor changes to initial commit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var engine = require('ejs-locals');
 var app = express();
 var port = process.env.PORT || 3000;
 app.get('/',function(req,res,next){
-	
+	res.send("Hello");
 });
 app.listen(port,function()
 {
