5a66525f9e238ec558776f7308f70579f2f8ab38
karangoel16 <karangoel16@yahoo.com>
2017-05-28 20:23:14 +0530

    Some more edits

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 var express = require('express');
 var engine = require('ejs-locals');
+var mongoose = require("mongoose");
+var path = require('path');
 var app = express();
+require('dotenv').load();
+
 var port = process.env.PORT || 3000;
+
+
+app.set('views', path.join(__dirname, '/views'));
+app.engine('ejs', engine);
+app.set('view engine', 'ejs');
+
 app.get('/',function(req,res,next){
 	res.send("Hello");
 });
+
 app.listen(port,function()
 {
 	console.log("listening at"+port);
